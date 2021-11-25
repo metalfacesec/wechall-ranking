9c226e54560356866d687437221f42be955497ed
Git <metalfacesec@gmail.com>
2021-11-24 23:12:33 -0500

    Cleaning up and wrapping up Wechall ranking widget

--- src/components/WechallRank.js
+++ src/components/WechallRank.js
@@ -1,21 +1,51 @@
 import React from "react";
-import Wechall from '../Wechall';
+
+import '../css/WechallRank.css';
 
 class WechallRank extends React.Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			rating: 'Loading...',
+			lastUpdated: this.getDateString()
+		}
+
+		this.updateRank = this.updateRank.bind(this);
+	}
+
+	getDateString() {
+		return `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString('en-US', { hour: "numeric", minute: "numeric"})}`;
+	}
+
+	updateRank() {
+		fetch(`http://localhost:8085/api/rank?profile=${this.props.profile}`, {crossDomain:true})
+		.then(response => response.json())
+		.then(response => {
+			if (typeof response.data !== "string") {
+				return;
+			}
+
+			this.setState({rating: response.data, lastUpdated: this.getDateString()});
+		})
+		.catch(err => {
+			console.log(err);
+		});
+	}
+
 	componentDidMount() {
-		Wechall.getRanking('metalface');
+		this.updateRank();
 	}
 
 	render() {
 		return (
 			<div>
-				<div id="rank" className="center">4290</div>
-				<div id="text" className="center">Current Global Wechall Ranking</div>
-				<div id="updated" className="center">Last updated 12/24/2021</div>
+				<div id="rank" className="center">{this.state.rating}</div>
+				<div id="text" className="center">Current Global Wechall Ranking For {this.props.profile}</div>
+				<div id="updated" className="center">Last updated {this.state.lastUpdated}</div>
 			</div>
 		);
 	}
 }
 
 export default WechallRank;
-
